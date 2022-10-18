193c0f0539158766fba00d34d826531170d8393e
Yulon <yulon@me.com>
2022-10-18 11:25:58 +0800

    feat: add respak{}

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -5,7 +5,8 @@
 
 int main(int argc, char const *argv[]) {
 	RUA_CHECK(argc, ==, 3);
-	RUA_CHECK(respak::load(rua::l2u(argv[2])), ==, "");
-	RUA_CHECK(respak::pack(rua::l2u(argv[1])), ==, "");
+	respak res;
+	RUA_SUCCESS(!res.load(rua::l2u(argv[2])));
+	RUA_SUCCESS(!res.pack(rua::l2u(argv[1])));
 	return 0;
 }
